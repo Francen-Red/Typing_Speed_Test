5b0614da0bd2eb2eaa602e6c72d87dfada486064
Francen-Red <francenvenissered@gmail.com>
2024-10-02 06:51:19 +0800

    Display the target text that the user will copy for the typing speed test

--- typingtest.py
+++ typingtest.py
@@ -4,8 +4,8 @@ import curses
 # Initialize and clean up the environment 
 from curses import wrapper
 
-#Add a welcoming remark string as a start off of the test
-#Ask for the user's response to start the game by typing any key to begin (/n) is used to move this on the next line
+# Add a welcoming remark string as a start off of the test
+# Ask for the user's response to start the game by typing any key to begin (/n) is used to move this on the next line
 def start_screen(stdscr):
     stdscr.clear()
     stdscr.addstr("Welcome to Red’s Typing Test! Practice your typing and break your own speed limit!") 
@@ -13,17 +13,29 @@ def start_screen(stdscr):
     stdscr.refresh()                                #Refresh the screen to show the added text
     stdscr.getkey ()   
     
+# Display the target text for the typing test, clear the screen, and wait for user input.    
+def wpm_test(stdscr): 
+    target_text = "Hi! This is just some text for this test"     #Load the target text
+    current_text = []                              #Initialize an empty list for the user's input
 
-# Setting up the standard screen for outputrd
+    stdscr.clear()
+    stdscr.addstr(target_text)     
+    stdscr.refresh()                                #Refresh the screen to show the added text
+    stdscr.getkey ()   
+    
+
+
+
+
+# Main function initializes the color pairs 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    stdscr.addstr("Hiii! This is just some text for this test!", curses.color_pair(2)) 
-    stdscr.refresh()                              
-    key = stdscr.getkey () 
-    print(key) 
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
+
 
 wrapper(main)
