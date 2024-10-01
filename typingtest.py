52d985789f231258705ba76ab48625eed35db5ef
Francen-Red <francenvenissered@gmail.com>
2024-10-01 21:12:16 +0800

    Set up color pairs for text output

--- typingtest.py
+++ typingtest.py
@@ -6,7 +6,11 @@ from curses import wrapper
 
 # Setting up the standard screen for output
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
     stdscr.clear()
-    stdscr.addstr("Hiii! This is just some text for this test!") 
+    stdscr.addstr("Hiii! This is just some text for this test!", curses.color_pair(2)) 
     stdscr.refresh()                              
     stdscr.getkey ()  
+
+wrapper(main)
